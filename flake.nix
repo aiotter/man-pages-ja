cbca572de511f607e0d80c96bce241f4ae93e7ed
aiotter <git@aiotter.com>
2022-10-11 02:48:21 +0900

    fix: Add configulation options

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             src = man-pages-ja;
             unpackCmd = "tar xvzf $curSrc";
             nativeBuildInputs = with pkgs; [ perl ];
-            buildInputs = with pkgs; [ makeWrapper groff ];
+            buildInputs = with pkgs; [ groff less coreutils less gzip bzip2 ];
 
             patchPhase = ''
               cp script/configure.perl{,.orig}
@@ -48,25 +48,45 @@
               yes "" | make config
             '';
 
-            postInstall = ''
-              # The manpath executable looks up manpages from PATH. And this package won't
-              # appear in PATH unless it has a /bin folder
-              mkdir -p $out/bin
+            postInstall =
+              let
+                configFile = pkgs.writeTextFile {
+                  name = "man.conf";
+                  text = ''
+                    TROFF   ${pkgs.groff}/bin/groff -Tps -mandoc -c
+                    NROFF   ${pkgs.groff}/bin/groff -Wall -mtty-char -Tascii -mandoc -c
+                    JNROFF  ${pkgs.groff}/bin/groff -Dutf8 -Tutf8 -mandoc -mja -E
+                    EQN     ${pkgs.groff}/bin/eqn -Tps
+                    NEQN    ${pkgs.groff}/bin/eqn -Tascii
+                    JNEQN   ${pkgs.groff}/bin/eqn -Tutf8
+                    TBL     ${pkgs.groff}/bin/tbl
+                    REFER   ${pkgs.groff}/bin/refer
+                    PIC     ${pkgs.groff}/bin/pic
+                    PAGER   ${pkgs.less}/bin/less -isr
+                    BROWSER ${pkgs.less}/bin/less -isr
+                    CAT     ${pkgs.coreutils}/bin/cat
 
-              mkdir -p $out/etc
-              echo "JNROFF ${pkgs.groff}/bin/groff -Dutf8 -Tutf8 -mandoc -mja -E" >$out/etc/man.conf
+                    .gz  ${pkgs.gzip}/bin/gunzip -c
+                    .bz2 ${pkgs.bzip2}/bin/bzip2 -cd
+                  '';
+                };
+              in
+              ''
+                # The manpath executable looks up manpages from PATH. And this package won't
+                # appear in PATH unless it has a /bin folder
+                mkdir -p $out/bin
 
-              cat <<EOF >"$out/bin/jaman"
-              #!${pkgs.runtimeShell}
-              MANPATH=$out/share/man LANG=ja_JP.UTF-8 \$(which man) "-C$out/etc/man.conf" "\$@"
-              EOF
-              chmod +x "$out/bin/jaman"
+                cat <<EOF >"$out/bin/jaman"
+                #!${pkgs.runtimeShell}
+                MANPATH=$out/share/man LANG=ja_JP.UTF-8 \$(which man) "-C${configFile}" "\$@"
+                EOF
+                chmod +x "$out/bin/jaman"
 
-              # makeWrapper /usr/bin/man "$out/bin/jaman" \
-              #   --set MANPATH $out/share/man \
-              #   --set LANG ja_JP.UTF-8 \
-              #   --add-flags "-C$out/etc/man.conf"
-            '';
+                # makeWrapper /usr/bin/man "$out/bin/jaman" \
+                #   --set MANPATH $out/share/man \
+                #   --set LANG ja_JP.UTF-8 \
+                #   --add-flags "-C$out/etc/man.conf"
+              '';
 
             outputDocdev = "out";
 
